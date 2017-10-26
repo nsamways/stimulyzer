6f60141184bdf341273d2137eb53835e475a63ef
nsamways <nsamways@localhost>
2017-10-26 23:48:27 +0100

    first implimentation of config parser

--- stimulyzer/test.py
+++ stimulyzer/test.py
@@ -6,27 +6,33 @@ Created on 25 Oct 2017
 #!/usr/bin/env
 
 from PIL import Image, ImageDraw
+from ConfigParser import ConfigParser
 import random
 
 def main():
-    
-    base_name = "stimuli_"
-    
-    stim_size = width, height = 640, 480
-    border_bound = 10
-    
-    object_number = 30
-    background_colour = "green"
-    set_size = 10
-    
-    object_size = 50
-    
-    shape_1_colour = "red"
 
+# read in the config file as passed by CLAs
+
+    cfg = ConfigParser()
+    cfg.read('config.ini')
+#     print(cfg.sections())
+#     print(cfg.items('Base Parameters')) 
+#     print(cfg.get('Items1','Colour'))
+    
+    base_name = cfg.get('Base Parameters','Base Name')
+    stim_size = width, height = cfg.getint('Base Parameters','Width'), cfg.getint('Base Parameters', 'Height');
+    base_filepath = cfg.get('Base Parameters','Directory Path')
+    background_colour = cfg.get('Base Parameters','BG Colour')
+    set_size = cfg.getint('Base Parameters','Number of stimuli')
+
+    
+    object_number = cfg.getint('Items1','Number')
+    object_size = cfg.getint('Items1', 'Bounding Size')
+    shape_1_colour = cfg.get('Items1','Colour')
+    fill = cfg.getboolean('Items1', 'Fill')
+    
     for j in range(set_size):
 
-        base_filepath = '/home/nsamways/stims/'
-        
         image = Image.new( "RGB" , stim_size, background_colour )
         stim = ImageDraw.Draw(image);   
 
@@ -39,14 +45,13 @@ def main():
             y1 = random.randint(0, (height - object_size));
             y2 = y1 + object_size;
         
-            stim.rectangle(((x1, y1),(x2,y2)), fill = shape_1_colour, outline = shape_1_colour)
+            if (fill):
+                stim.rectangle(((x1, y1),(x2,y2)), fill = shape_1_colour, outline = shape_1_colour)
+            else:
+                stim.rectangle(((x1, y1),(x2,y2)), outline = shape_1_colour)
 
 
         stim_file_name = base_name + str(j) + ".jpg" 
-    #draw.ellipse((0, 0, 180, 180), fill = 'white', outline ='blue')
-    # draw.rectangle(((20,20),(40,40)), fill = 'white', outline = 'blue')
-    #draw.rectangle(((0, 00), (100, 100)), fill="black")
-#       image.show();
 
         image.save(base_filepath + stim_file_name)
 
