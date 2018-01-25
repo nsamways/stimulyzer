691f4249f85f61ce1e57da121f6b6ebf18608197
nsamways <nsamways@localhost>
2018-01-25 21:51:07 +0000

    Main rearrangement for new format

--- stimulyzer/test.py
+++ stimulyzer/test.py
@@ -6,38 +6,27 @@ Created on 25 Oct 2017
 #!/usr/bin/env
 
 from PIL import Image, ImageDraw
+
 from ConfigParser import ConfigParser
+
 import random
 
 def main():
 
 # read in the config file as passed by CLAs
+    ( base_config, object_list ) = get_configuration()
+    
 
-    cfg = ConfigParser()
-    cfg.read('config.ini')
-#     print(cfg.sections())
-#     print(cfg.items('Base Parameters')) 
-#     print(cfg.get('Items1','Colour'))
+    # calculate the number of loci, total number of objects etc.
     
-    base_name = cfg.get('Base Parameters','Base Name')
-    stim_size = width, height = cfg.getint('Base Parameters','Width'), cfg.getint('Base Parameters', 'Height');
-    base_filepath = cfg.get('Base Parameters','Directory Path')
-    background_colour = cfg.get('Base Parameters','BG Colour')
-    set_size = cfg.getint('Base Parameters','Number of stimuli')
-
-    
-    object_number = cfg.getint('Items1','Number')
-    object_size = cfg.getint('Items1', 'Bounding Size')
-    shape_1_colour = cfg.get('Items1','Colour')
-    fill = cfg.getboolean('Items1', 'Fill')
-    
+        
     for j in range(set_size):
 
         image = Image.new( "RGB" , stim_size, background_colour )
         stim = ImageDraw.Draw(image);   
 
         for i in range(object_number):
-
+        
         # create a random coordinate bounded by min and max
             x1 = random.randint(0, (width - object_size));
             x2 = x1 + object_size
@@ -65,4 +54,55 @@ def main():
 if ( __name__ == "__main__"):
     
     main();
+
+def get_configuration( conf_filename = "config.ini" ):
+
+# set up the members
+    base
+
+    cfg = ConfigParser()
+    cfg.read( conf_filename )
     
+    # get all of the different sections    
+     
+    all_cfg_sections = cfg.sections()
+#     print(cfg.items('Base Parameters')) 
+#     print(cfg.get('Items1','Colour'))
+
+    for each_section in (all_cfg_sections):
+        print(" processing section:" + each_section)
+        
+        # read in the base parameters
+        if  "Base Parameters" in each_section:
+            # list through parameters
+            pass
+            
+        elif "Items" in each_section:
+            # add the item type to the      
+            
+            
+    # read in the base parameters
+    base_name = cfg.get('Base Parameters','Base Name')
+    stim_size = width, height = cfg.getint('Base Parameters','Width'), cfg.getint('Base Parameters', 'Height');
+    base_filepath = cfg.get('Base Parameters','Directory Path')
+    background_colour = cfg.get('Base Parameters','BG Colour')
+    set_size = cfg.getint('Base Parameters','Number of stimuli')
+
+    
+    object_number = cfg.getint('Items1','Number')
+    object_size = cfg.getint('Items1', 'Bounding Size')
+    shape_1_colour = cfg.get('Items1','Colour')
+    fill = cfg.getboolean('Items1', 'Fill')
+    
+return(base_params, objects)
+
+def get_base_parameters():
+
+def paint_polygons():
+    
+return()
+
+def paint_circles():
+    
+return()
+
