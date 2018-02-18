50fe9561744e90147fecac0817fffaf7a1cb93d2
nsamways <nsamways@localhost>
2018-02-18 21:27:39 +0000

    Major changes; coord and shape matrices complete (but not tested)

--- stimulyzer/stimulyzer.py
+++ stimulyzer/stimulyzer.py
@@ -10,14 +10,21 @@ from PIL import Image, ImageDraw
 from ConfigParser import ConfigParser
 
 import random, glob, math 
+from docutils.utils.math.tex2unichar import space
+
+
+
 
 def main():
+
+    # set some vars
+    total_objects_count = []
 
     # read in the config file as passed by CLAs
     ( base_config, distractor_list, target_parameters ) = get_configuration()   
     
     # check if there is a target present
-    if bool(target.parameters()):
+    if bool(target_parameters()):
         target_present = True
     else:
         target_present = False
@@ -26,102 +33,130 @@ def main():
     stim_width = int(base_config["width"])
     stim_height = int(base_config["height"])
     stim_set_size = int(base_config["number of stimuli"])
+    spacer = int(base_config["padding"])
     
     # determine some variables
     stim_size = [stim_width, stim_height]
             
     # calculate the size of the matrix, the offsets etc.
     
-    
     # first calculate the maximum cell size, based on the largest size of object plus padding
     # also get the total number of objects per stimuli
     
-    maximum_stim_radius = 0
-    total_objects_per_stim = 0 
+    maximum_stim_radius = 0 
     
     i = 0 # enumerator
     for shapes in distractor_list:
         # get the size of current distractor 
-        i+=1
-        cur_size = int(shapes["radius"])
-        total_objects_per_stim += int(shapes["number"]) 
-        if  cur_size > maximum_stim_radius:
-            maximum_stim_radius = cur_size
+        curr_radius = int(shapes["radius"])
+        total_objects_count.append(int(shapes["number"])) 
+        if  curr_radius > maximum_stim_radius:
+            maximum_stim_radius = curr_radius
     
-    # do for target, if present
+    # do also for target, if present
     if (target_present):
     
         if int(target_parameters["radius"]) > maximum_stim_radius:
-            maximum_stim_radius = int(target_parameters["number"])
+            total_objects_count.append(int(shapes["number"])) 
     
-        total_objects_per_stim += int(target_parameters["number"])
         
-    max_cell_height = max_cell_width = maximum_stim_radius + int(base_config["padding"])
-
+    max_cell_height = max_cell_width = 2 * maximum_stim_radius + spacer
 
     objects_per_width = math.floor( stim_width / max_cell_width)
     objects_per_height = math.floor( stim_height / max_cell_height )
 
     # now check that the total obs' per width and height, PLUS the extra spacer fits within the stimuli size
-    if (objects_per_width * max_cell_width + int(base_config["padding"])) > stim_width:
+    if (objects_per_width * max_cell_width + spacer) > stim_width:
         print("Horizontal padding overlap. Reducing cells per width by 1")
         objects_per_width -= 1
     
-    if (objects_per_height * max_cell_height + int(base_config["padding"])) > stim_height:
+    if (objects_per_height * max_cell_height + spacer) > stim_height:
         print("Verticle padding overlap. Reducing cells per width by 1")
         objects_per_height -= 1
+    # we also need to check that there are an even number of cols, else we can't have left/right targets
+    
+    # now we have the max number of slots, we can work out the offsets:
+    horizontal_offset = math.floor(( stim_width - (objects_per_width * max_cell_width + spacer)) / 2 )
+    verticle_offset = math.floor(( stim_height - (objects_per_height * max_cell_height + spacer)) / 2 )
+        
         
     # calculate the number of loci, based on total number of objects etc.
     
     total_loci = objects_per_height * objects_per_width
     
-    # calculate the number of empy loci
-    num_spacers = total_loci - total_objects_per_stim
+    # calculate the number of empty loci
+    num_spacers = total_loci - sum(total_objects_count)
+
+    if target_present:
+        num_spacers -= int(target_parameters["number"])
 
     # check there are less objects than loci
     if num_spacers < 0:
         print("There are more objects than available slots. Exiting.")
         exit()
-         
-    # create a list of distractors in the form: [[type, number],[type, number]]
-    for current_distractor_idx in range(len(distractor_list)):
-        number_of_current_distractor = (distrator_list[current_distractor_idx][""])
-    
-    # append spacers
-    
-    # now add on the extra padding for symmet
+           
+    # now add on the extra padding for symmetry
     print("Target present:" + str(target_present))
     print("Objects per width:" + str(objects_per_width))
     print("Objects per height:" + str(objects_per_height))
     print("Max stim radius:" + str(maximum_stim_radius))   
     print("Max cell height:" + str(max_cell_width))   
-    print("Total objects:" + str(total_objects_per_stim))
+    print("Total objects:" + str(sum(total_objects_count)))
     print("Total loci" + str(total_loci))
     
     # create the base_shape and coordinate matrices
     base_shape_matrix = []
-    coord_matrix = []
+    
+    for object_types in range(len(total_objects_count)):
+        for j in range(total_objects_count[object_types]):
+            base_shape_matrix.append(object_types)
+    # now push on the spacers as value: -1
+    for instances in range(num_spacers):
+        base_shape_matrix.append(-1)
     
     
+    # we will use shuffled copies of the base_shape_matrix for the stims as this will remain the same throughout each iteration
+
+    coord_matrix = [] 
     
     # loop through matrix, giving x,y coordinates for each loci
-    for mat_rows in range():
-        for mat_cols in range():
-            # calculate and push the x,y coordinate tuple
-            
+    
+    # set up the x,y list
+    coord_pair = []
+    
+    for mat_rows in range(objects_per_height):
+        for mat_cols in range(objects_per_width):
+            # calculate and push the x,y coordinate tuple on to coord matrix
+            # nb. add: offset + one spacer + radius + (2 * radius + spacer)
+            coord_pair = ((horizontal_offset + maximum_stim_radius + spacer + (mat_cols * (2 * maximum_stim_radius + spacer))),(verticle_offset + maximum_stim_radius + spacer + (mat_rows * (2 * maximum_stim_radius + spacer))) ) 
+            coord_matrix.append(coord_pair) # push the current pair on the the vector    
+
+    # now we have the shape matrix (well, vector), and the coordinate matrix (well, vector) so can start drawing the shapes
+
+    # we need to push the target on to the distractor list now because we need to pass the parameters to the 'draw_polygon' function when the target is called.
+         
+    distractor_list.append(target_parameters)
     
     # Main loop for creation of all stimuli        
     for j in range(stim_set_size):
         # we do this for a single stimuli (image)
         
-        # dupliceate the base_shape matrix, then shuffle it for this stimulus
-        current_shape_matrix = base_shape_matrix
+        # duplicate the base_shape matrix, then shuffle it for this stimulus
+        current_shape_matrix = base_shape_matrix        
+        random.shuffle(current_shape_matrix)
         
-        random.shuffle(current_shape.matrix)
-        
-        # add in the distractor(s) NOTE: we do this now because we need to ensure it is places correctly
-        
-        draw_po
+        # add in the distractor(s) NOTE: we do this now because we need to ensure it is placed correctly
+        if (target_present):
+            # check if position is left or right
+            if ("left" in target_parameters["position"]):
+                # make sure the target is appended on the left.
+                
+                
+            elif ("right" in target_parameters):
+                # make sure the target is appended to the right.
+            else:
+                print("The target position is not specified, so appending randomly")
+                current_shape_matrix.insert(randint(0,len(current_shape_matrix)), len(total_objects_count)) #  NOTE: len(total_objects_count) will give the next number in the sequence
         
         
         global stim
@@ -129,25 +164,16 @@ def main():
         image = Image.new( "RGB" , stim_size, background_colour )
         stim = ImageDraw.Draw(image)
 
-
-        # populate the shape matrix NB: the coord matrix remains the same for all stims'
+        # loop through for each individual shape in the shape matrix
+        for individual_shape in range(len(shape_matrix)):
+            if current_shape_matrix[individual_shape] < 0:
+                pass # this is a spacer
+            else:
+                # pass on the centroid coordinates and shape_type to the draw_polygons function
+                draw_polygons(coordinate_matrix[individual_shape], distractor_list[current_shape_matrix[individual_shape])
         
-        # push all the distractors in to the matrix
-        
-        # push the 'spaces' on
-        
-        # shuffle the matrix (randomise it)
-        
-        # add the target, depending on L or R
-        
-        # traverse the shape matrix, passing to correct function for drawing on  
-
-   
-        
-        # loop through for each individual stimulus
-        for i in range(object_number):
-            pass
-        
+        # this should have now drawn all the shapes on.
+                
         # set the filename and save the file to the appropriate path / name based on base name and current iteration        
         stim_file_name = str(base_config["Base Name"]) + "_" + str(j) + ".jpg" 
         
@@ -213,21 +239,24 @@ def get_configuration( conf_filename = "config.ini" ):
 
 
 
-def paint_polygons(x_coord, y_coord, shape_info):
+def paint_polygons(shape_coordinates, shape_info):
      
     # set up the vertex list
     vertices = []
      
     # get some 
-    poly_radius = int ( shape_info[  ])
+    poly_radius = int ( shape_info["radius"  ])
+    poly_vertices = int(shape_info["vertices"])
+    
+    # calculate the internal angles
+    internal_angle = poly_vertices / (2 * math.pi)
      
     # create the list of vertices
     for ( i in range (int(shape_info["Vertices"]))):
         # create the points as pairs
          
-        # x coord
-        xi = x_coord + math.sin()
-        yi = y_coord + math.cos()
+        xi = shape_coordinates[0] + math.ceil((poly_radius * math.sin( i * internal_angle)))
+        yi = shap_coordinates[1] + math.ceil((poly_radius * math.cos( i * internal_angle)))
          
         point_pair = [xi, yi]
          
@@ -236,9 +265,12 @@ def paint_polygons(x_coord, y_coord, shape_info):
  
     # draw this polygon on to the stimulus image
      
-    stim.polygon()
+    stim.polygon(vertices,)
          
 return()
+
+
+def write_AOIs(coord_list, shape_:
         # create a random coordinate bounded by min and max
             x1 = random.randint(0, (width - object_size));
             x2 = x1 + object_size
